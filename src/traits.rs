e52d1a37ba319fe9ec51c4a506dd2db7a03949d2
sshwy <jy.cat@qq.com>
2026-01-07 14:29:29 +0800

    feat: implement SerdeSchema trait for tuples and unit type

--- src/traits.rs
+++ src/traits.rs
@@ -3,3 +3,32 @@ use crate::expr::TypeExpr;
 pub trait SerdeSchema {
     fn serde_schema() -> TypeExpr;
 }
+
+impl SerdeSchema for () {
+    fn serde_schema() -> TypeExpr {
+        TypeExpr::Unit
+    }
+}
+
+macro_rules! impl_serde_schema_for_tuple {
+    ($($T:ident),+ $(,)?) => {
+        impl<$($T),+> SerdeSchema for ($($T,)+)
+        where
+            $($T: SerdeSchema),+
+        {
+            fn serde_schema() -> TypeExpr {
+                TypeExpr::Tuple {
+                    elements: vec![$(<$T as SerdeSchema>::serde_schema()),+],
+                }
+            }
+        }
+    };
+}
+
+impl_serde_schema_for_tuple!(T0);
+impl_serde_schema_for_tuple!(T0, T1);
+impl_serde_schema_for_tuple!(T0, T1, T2);
+impl_serde_schema_for_tuple!(T0, T1, T2, T3);
+impl_serde_schema_for_tuple!(T0, T1, T2, T3, T4);
+impl_serde_schema_for_tuple!(T0, T1, T2, T3, T4, T5);
+impl_serde_schema_for_tuple!(T0, T1, T2, T3, T4, T5, T6);
